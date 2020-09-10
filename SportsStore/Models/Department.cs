d82ac4362b938358d319386e714fb7d6598bf4aa
Bas <asberyan@gmail.com>
2020-09-10 23:39:22 +0400

    Add project

---
+++ SportsStore/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models {
+    public class Department {
+
+        public long Departmentid { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<Person> People { get; set; }
+    }
+}
